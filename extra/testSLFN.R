24ab73db2697e1b27881e9725ab9dc70bec5c10f
maarten (Dr ) <m.speekenbrink@ucl.ac.uk>
2010-03-03 00:06:00 +0000

    generalized slfn.c
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mcplr@123 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- extra/testSLFN.R
+++ extra/testSLFN.R
@@ -1,21 +1,31 @@
 slfn <- function(y,x){
 
-	y <- rbinom(10,size=1,prob=.7)
-	x <- rbind(1,matrix(rnorm(10*3),nrow=3))
-	eta <- runif(4)
+  ny <- 1
+	if(ny > 1) {
+	  y <- matrix(runif(100*ny),nrow=ny)
+	  y <- apply(y,2,function(x) as.numeric(x == max(x)))
+	} else {
+	  y <- rbinom(100,size=1,prob=.7)
+	}
+	if(!is.matrix(y)) y <- matrix(y,ncol=length(y))
+	x <- rbind(1,matrix(rnorm(100*3),nrow=3))
+	eta <- matrix(runif(4*ny),ncol=ny)
 	
 	dyn.load("~/Documents/RForge/mcplr/trunk/src/slfn.so")
 	
-	w <- matrix(0.0,ncol=ncol(x)+1,nrow=nrow(x))
+	w <- array(0.0,dim=c(nrow(x),nrow(y),ncol(x)+1))
 	
-	output <- .C("slfn_logistic_1y",
-	y = as.integer(y),
+	output <- .C("slfn",
+	y = as.double(y),
+	ny = as.integer(nrow(y)),
 	x = as.double(x),
 	nx = as.integer(nrow(x)),
 	nt = as.integer(ncol(x)),
 	eta = as.double(eta),
-	w = as.double(w))
-	result <- matrix(output$w,ncol=ncol(w),nrow=nrow(w))
+	actfun = as.integer(2),
+	w = as.double(w),
+	ypred = as.double(y))
+	result <- list(w=array(output$w,dim=c(nrow(x),NROW(y),ncol(x))),ypred=matrix(output$ypred,ncol=ncol(x),nrow=nrow(y)))
 	
 	return(result)
 	
--- trunk/src/slfn.c
+++ trunk/src/slfn.c
@@ -1,35 +1,59 @@
-#include <R.h>
+// #include <R.h>
 #include <Rmath.h>
 
-void slfn_logistic_1y(int *y, double *x, int *nx, int *nt, double *eta, double *w)
+void slfn(double *y, int *ny, double *x, int *nx, int *nt, double *eta, int *actfun, double *w, double *ypred)
 {
   /*
-  y = nt*1 vector
+  y = ny*nt matrix
   x = nx*nt matrix
-  w = nx*nt matrix
+  eta = nx*ny matrix
+  actfun: 1 = linear, 2 = logistic, 3 = softmax
+  w = nx*ny*nt matrix
+  ypred = ny*nt matrix
   */ 
  
-  int i,t;
-  double ypl = 0.0;
-  double yp = 0.0;
+  int i,j,t;
+  int dim1 = *nx;
+  int dim2 = *nx * *ny;
+  double ypl[*ny];
+  double syp = 0.0;
   
   // make prediction
-  for(i=0; i < *nx; i++) {
-      ypl += w[i] * x[i];
-   }
-   yp = 1/(1+exp(-1 * ypl));
-    
-  for(t=0; t < *nt; t++) {
-  	ypl = 0.0;
-    for(i=0; i < *nx; i++) {
-    
-      // update weights
-      
-      w[i + (t+1) * *nx] = w[i + t * *nx] - eta[i]*(y[t] - yp)*x[i + t * *nx] ;
-      // make prediction for next trial
-      ypl += w[i + t * *nx] * x[i + t * *nx] ;
+  for(j=0; j < *ny; j++)
+  {
+    ypl[j] = 0.0;
+    for(i=0; i < *nx; i++) { ypl[j] += w[i + j*dim1] * x[i]; }
+    if(*actfun == 1) { ypred[j] = ypl[j]; }
+    if(*actfun == 2) { ypred[j] = 1/(1+exp(-1 * ypl[j])); }
+    if(*actfun == 3) 
+    { 
+      ypred[j] = exp(ypl[j]); 
+      syp += ypred[j];
     }
-    yp = 1/(1+exp(-1 * ypl));
   }
-
+  if(*actfun == 3) for(j=0; j < *ny; j++) { ypred[j] = ypred[j]/syp; }
+    
+  for(t=0; t < *nt; t++) 
+  {
+    syp = 0.0;
+    for(j = 0; j < *ny; j++)
+    {
+  	  ypl[j] = 0.0;
+      for(i=0; i < *nx; i++) 
+      {
+        // update weights
+        w[i + j * dim1 + (t+1) * dim2] = w[i + j * dim1 + t * dim2] - eta[i + j * dim1]*(y[j + t * dim1] - ypred[t * *ny])*x[i + j * dim1 + t * dim2];
+        // make prediction for next trial
+        ypl[j] += w[i + j * dim1 + t * dim2] * x[i + j * dim1 + t * dim2];
+      }
+      if(*actfun == 1) { ypred[j + t * *ny] = ypl[j]; }
+      if(*actfun == 2) { ypred[j + t * *ny] = 1/(1+exp(-1 * ypl[j])); }
+      if(*actfun == 3) 
+      { 
+        ypred[j + t * *ny] = exp(ypl[j]);
+        syp += ypred[j];
+      }
+    }
+    if(*actfun == 3) for(j=0; j < *ny; j++) { ypred[j] = ypred[j]/syp; }
+  }
 }
