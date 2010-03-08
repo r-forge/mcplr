2773bf35becb6b5e788136526085ac503171f036
maarten (Dr ) <m.speekenbrink@ucl.ac.uk>
2010-03-08 17:58:10 +0000

    - changes to gcm
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mcplr@125 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/src/gcm.c
+++ trunk/src/gcm.c
@@ -1,7 +1,7 @@
 // #include <R.h>
 #include <Rmath.h>
 
-void gcm_nominal(int *y, int *ny, double *x, int *nx, int *nt, double *w, double *r, double *q, double *lambda, double *gamma, double *dist, double *sim, double *ypred)
+void gcm_nominal(int *y, int *ny, double *x, int *nx, int *bt, int *et, int *lt, double *w, double *r, double *q, double *lambda, double *gamma, double *dist, double *sim, double *ypred)
 {
   /*
   y = ny*nt matrix
@@ -11,31 +11,42 @@ void gcm_nominal(int *y, int *ny, double *x, int *nx, int *nt, double *w, double
   sim = ny vector
   ypred = ny*nt matrix
   */ 
-  int i,j,t,tt;
+  // int i,j,t,tt;
   double syp = 0.0;
      
-  for(t=1; t < *nt; t++) 
-  {
-    for(j=0; j < *ny; j++) sim[j] = 0.0;
-    for(tt=0; tt < t; tt++)
-    {
-      // compute distance
-      dist[tt - 1] = 0.0;
-      for(i=0; i < *nx; i++) dist[tt-1] += w[i] * pow( fabs(x[i + tt * *nx] - x[i + t * *nx ]), *r);
-      dist[tt - 1] = pow(dist[tt-1],1 / *r);
-      // compute similarity and add to sim
-      for(j=0; j < *ny; j++)
-      {
-        if(y[j + tt * *ny] == 1) sim[j] += exp( - *lambda * pow(dist[tt - 1],*q));
-      }
-    }
-    // make prediction
-    syp = 0.0;
-	for(j=0; j < *ny; j++)
-	{
-	  ypred[j + t * *ny] = pow(sim[j], *gamma);
-	  syp += ypred[j + t * *ny];
-	}
-    for(j=0; j < *ny; j++) ypred[j + t * *ny] = ypred[j + t * *nt]/syp;
+  for(int k=0; k < *lt; k++) {   
+	  for(int t=bt[k]; t < et[k]; t++) 
+	  {
+	    for(int j=0; j < *ny; j++)
+	    { 
+	      sim[j] = 0.0;
+	    }
+	    for(int tt=bt[k]-1; tt < t; tt++)
+	    {
+	      // compute distance
+	      dist[tt] = 0.0;
+	      for(int i=0; i < *nx; i++)
+	      {
+	        dist[tt] += w[i] * pow( fabs(x[i + tt * *nx] - x[i + t * *nx ]), *r);
+	      }
+	      dist[tt] = pow(dist[tt],*q / *r);
+	      // compute similarity and add to sim
+	      for(int j=0; j < *ny; j++)
+	      {
+	        if(y[j + tt * *ny] == 1) sim[j] += exp(*lambda * dist[tt]);
+	      }
+	    }
+	    // make prediction
+	    syp = 0.0;
+		for(int j=0; j < *ny; j++)
+		{
+		  ypred[j + t * *ny] = pow(sim[j], *gamma);
+		  syp += ypred[j + t * *ny];
+		}
+	    for(int j=0; j < *ny; j++)
+	    {
+	      ypred[j + t * *ny] = ypred[j + t * *ny] / syp;
+	    }
+	  }
   }
 }
